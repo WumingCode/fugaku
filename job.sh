0a314a0e586b1afbb16ad8eceb4289cd8ea25ac0
Yosuke Matsumoto <38919635+ymatumot@users.noreply.github.com>
2021-08-07 08:55:10 +0900

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-#PJM --name "em2d_test"
-#PJM -L  "node=1"                      # 割当ノード数 12 （3次元形状）
-#PJM -L  "rscgrp=small"                    # リソースグループの指定
-#PJM -L  "elapse=01:00:00"                 # 経過時間制限 1時間
-#PJM --mpi "max-proc-per-node=4"           # 1ノードあたりに生成するMPIプロセス数の上限値
+#PJM --name "test"
+#PJM -L  "node=900"              # 割当ノード数900
+#PJM -L  "rscgrp=large"          # リソースグループの指定
+#PJM -L  "elapse=12:00:00"       # 経過時間制限 12時間
+#PJM --mpi "max-proc-per-node=4" # 1ノードあたりに生成するMPIプロセス数の上限値
 #PJM -o sys_out
 #PJM -e sys_err
 
-export OMP_NUM_THREADS=12        # スレッド数の指定
-export PLE_MPI_STD_EMPTYFILE=OFF
+export OMP_NUM_THREADS=12         # スレッド数の指定
+export PLE_MPI_STD_EMPTYFILE=OFF  # 空の標準出力ファイルの生成の抑制
+export OMPI_MCA_plm_ple_memory_allocation_policy=bind_local  # CMGのメモリとプロセスをバインド
 
-. /vol0004/apps/oss/spack/share/spack/setup-env.sh
-spack load /7cxah5f
-export LD_LIBRARY_PATH=/lib64:$LD_LIBRARY_PATH
-
-mpiexec -n 4 -stdout sys_out -stderr sys_err ./em2dsk_test.out
-
+mpiexec -n 3600 -stdout sys_out -stderr sys_err ./em2d_test.out　# 900 node x 4 MPI procs
